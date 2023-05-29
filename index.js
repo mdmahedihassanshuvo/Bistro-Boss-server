cab380a2b15dec5c27468d61eb991d151e2d440c
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-05-29 11:34:56 +0600

    cart delete method added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -28,13 +29,45 @@ async function run() {
 
         const menuCollection = client.db("bistrodb").collection("menu");
         const reviewCollection = client.db("bistrodb").collection("reviews");
+        const cartCollection = client.db("bistrodb").collection("carts");
 
-        app.get('/menu', async(req, res) => {
+        app.post('/carts', async (req, res) => {
+            const item = req.body
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email
+            // console.log(email)
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray()
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            const result = { token }
+            res.send(result)
+        })
+
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
 
-        app.get('/review', async(req, res) => {
+        app.get('/review', async (req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result);
         })
