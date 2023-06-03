73a627eb9b0ff62b5eef8990f25ccbe2cb597611
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-06-03 19:47:11 +0600

    add verify admin and json web token

--- index.js
+++ index.js
@@ -148,6 +148,19 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/menu', verifyJwt, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+        app.delete('/menu/:id',verifyJwt, verifyAdmin, async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.get('/review', async (req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result);
